5c076754d38169ac7765471860595f00783f0ede
ondrejcervan <99639605+ondrejcervan@users.noreply.github.com>
2024-04-15 11:37:30 +0200

    final commit

--- searching.py
+++ searching.py
@@ -59,6 +59,20 @@ def pattern_search(sequence, pattern):
 
     return positions
 
+def binary_search(numbers, target):
+    left, right = 0, len(numbers) - 1
+
+    while left <= right:
+        mid = (left + right)//2
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return None
+
+
 
     
 def main():
